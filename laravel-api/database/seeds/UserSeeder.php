f1ab471b59f3407c999fb8abf796fb22c34b7cdf
rimantepomeckyte <rimante.pomeckyte@gmail.com>
2021-03-01 12:25:47 +0200

    added fake users

--- laravel-api/database/seeds/UserSeeder.php
+++ laravel-api/database/seeds/UserSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -20,6 +21,18 @@ class UserSeeder extends Seeder
                 'first_name' => 'Admin',
                 'last_name' => 'Admin',
                 'password' => Hash::make('admin123'),
+            ]);
+        }
+
+        $faker = Faker::create();
+        foreach (range(1,7) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker->userName,
+                'email' => $faker->email,
+                'first_name'=> $faker->firstName,
+                'last_name'=> $faker->lastName,
+                'address'=> $faker->address,
+                'password' => bcrypt('secret'),
             ]);
         }
     }
