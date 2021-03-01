3a43254040693b2ad4f23993e73567271a109854
vaidasuope <vaida.dabasinskaite@gmail.com>
2021-03-01 22:06:31 +0200

    registration

--- laravel-api/app/User.php
+++ laravel-api/app/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
 
     public function roles(){
 
-        return $this->belongsToMany(Role::class, 'role_users', 'role_id', 'user_id');
+        return $this->belongsToMany(Role::class, 'role_users', 'user_id','role_id');
     }
 
 
--- laravel-api/database/seeds/UserSeeder.php
+++ laravel-api/database/seeds/UserSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Role;
+use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -10,23 +12,27 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      *
-     * @return void
+     * @return User
      */
     public function run()
     {
         {
-            DB::table('users')->insert([
+            $admin= new User ([
                 'name' => 'Admin',
                 'email' => 'admin@smarteshop.com',
                 'first_name' => 'Admin',
                 'last_name' => 'Admin',
                 'password' => Hash::make('admin123'),
             ]);
+            $admin->save();
+            $role = Role::where('role', '=', 'Admin')->get('id');
+            $admin->roles()->sync(1,false);
+
         }
 
         $faker = Faker::create();
         foreach (range(1,7) as $index) {
-            DB::table('users')->insert([
+            $fakers = new User([
                 'name' => $faker->userName,
                 'email' => $faker->email,
                 'first_name'=> $faker->firstName,
@@ -34,6 +40,9 @@ class UserSeeder extends Seeder
                 'address'=> $faker->address,
                 'password' => bcrypt('secret'),
             ]);
+            $fakers->save();
+            $fakers->roles()->attach(3);
         }
     }
 }
+
