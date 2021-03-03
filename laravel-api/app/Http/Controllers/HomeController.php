f33a81f33e06e8593f924a3b5cac378636763cf2
Linas Pelenis <lpelenis@gmail.com>
2021-03-03 13:40:56 +0200

    admin login added full

--- laravel-api/app/Http/Controllers/HomeController.php
+++ laravel-api/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,6 +10,7 @@ class HomeController extends Controller
 
     public function index()
     {
+
         return view('welcome');
     }
 }
