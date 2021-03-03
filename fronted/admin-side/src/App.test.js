d342833aba3a98f156e640a57298b366030b7475
Martynas <alijosius.martynas@gmail.com>
2021-03-03 13:51:23 +0200

    admin-panel

--- fronted/admin-side/src/App.test.js
+++ fronted/admin-side/src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme/build'
+import App from './App'
+import ChartLineSimple from './views/charts/ChartLineSimple'
+import Dashboard from './views/dashboard/Dashboard.js'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+it('mounts App without crashing', () => {
+  const wrapper = shallow(<App/>)
+  wrapper.unmount()
+})
+
+it('mounts Dashboard without crashing', () => {
+  const wrapper = shallow(<Dashboard/>)
+  wrapper.unmount()
+})
+
+it('mounts Charts without crashing', () => {
+  const wrapper = shallow(<ChartLineSimple/> )
+  wrapper.unmount()
+})
--- fronted/admin-side/src/index.js
+++ fronted/admin-side/src/index.js
@@ -1,17 +1,27 @@
+import 'react-app-polyfill/ie11'; // For IE 11 support
+import 'react-app-polyfill/stable';
+import 'core-js';
+import './polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+
+import { icons } from './assets/icons'
+
+import { Provider } from 'react-redux'
+import store from './store'
+
+React.icons = icons
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
