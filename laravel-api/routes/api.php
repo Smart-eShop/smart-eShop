6ef9ab5cbc72f6f010e5a3f1a0e6e50b9d3008b8
Linas Pelenis <lpelenis@gmail.com>
2021-03-03 09:26:12 +0200

    admin controler pradzia

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('register', 'UserController@registerUser');
+//Admin login
+Route::post('login/admin', 'AdminController@adminLogin');
