521e63c072132ddd029623cb56eb7c7dabfdb081
rimantepomeckyte <rimante.pomeckyte@gmail.com>
2021-03-04 12:49:00 +0200

    added getUsers

--- laravel-api/app/Http/Controllers/HomeController.php
+++ laravel-api/app/Http/Controllers/HomeController.php
@@ -15,7 +15,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $users = User::with('roles')->get();
-        //dd(auth()->user());
+
         return view('welcome', compact('users'));
 
     }
