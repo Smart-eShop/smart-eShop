bb46b63424eebb2b6168049a0765ef6ef1d84556
laura.sadi <laura.sadinauskaite@gmail.com>
2021-03-10 14:23:16 +0200

    added fetch

--- fronted/admin-side/src/components/AddProduct.js
+++ fronted/admin-side/src/components/AddProduct.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Link, Redirect, Route, BrowserRouter as Router } from 'react-router-dom';
+import React, { useState } from 'react';
 
 import {
   CButton,
@@ -8,37 +7,47 @@ import {
   CCardFooter,
   CCardHeader,
   CCol,
-  CCollapse,
-  CDropdownItem,
-  CDropdownMenu,
-  CDropdownToggle,
-  CFade,
   CForm,
   CFormGroup,
   CFormText,
-  CValidFeedback,
-  CInvalidFeedback,
   CTextarea,
   CInput,
   CInputFile,
-  CInputCheckbox,
-  CInputRadio,
-  CInputGroup,
-  CInputGroupAppend,
-  CInputGroupPrepend,
-  CDropdown,
-  CInputGroupText,
   CLabel,
   CSelect,
-  CRow,
-  CSwitch
+  CRow
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import { DocsLink } from 'src/reusable'
+
 
 const AddProduct = () => {
-  const [collapsed, setCollapsed] = React.useState(true)
-  const [showElements, setShowElements] = React.useState(true)
+  console.log('Bandymas')
+  const AddProductFetch = e => {
+    fetch(`https://eshopsmart.herokuapp.com/api/addItem?title=${titleInput}&description=${descriptionInput}&keywords=${keywordsInput}&img=${imgInput}&price=${priceInput}&discount=${discountInput}&quantity=${quantityInput}&weight=${weightInput}&size=${sizeInput}`,{
+      method: "POST",
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+
+    }
+    )
+    .then(response => response.json())
+    .then(json =>{
+      console.log(json)
+    })
+  };
+  const [titleInput, setTitle] = useState('');
+  const [descriptionInput, setDescription] = useState('');
+  const [keywordsInput, setKeywords] = useState('');
+  const [imgInput, setImg] = useState([]);
+  const [priceInput, setPrice] = useState('');
+  const [discountInput, setDiscount] = useState('');
+  const [quantityInput, setQuantity] = useState('');
+  const [weightInput, setWeight] = useState('');
+  const [sizeInput, setsize] = useState('');
+  // const [collapsed, setCollapsed] = React.useState(true)
+  // const [showElements, setShowElements] = React.useState(true)
 
   return(
     <CRow>
@@ -54,10 +63,12 @@ const AddProduct = () => {
                 <CLabel htmlFor="text-input">Product name</CLabel>
               </CCol>
               <CCol xs="12" md="9">
-                <CInput id="text-input" name="text-input" placeholder="Text" />
+                <CInput id="text-input" name="text-input" placeholder="Text" value={titleInput} onInput={e =>setTitle(e.target.value)} />
                 <CFormText>This is a help text</CFormText>
               </CCol>
             </CFormGroup>
+
+
             <CFormGroup row>
               <CCol md="3">
                 <CLabel htmlFor="textarea-input">Product description</CLabel>
@@ -68,15 +79,18 @@ const AddProduct = () => {
                   id="textarea-input" 
                   rows="9"
                   placeholder="Content..." 
+                  value={descriptionInput} onInput={e =>setDescription(e.target.value)}
                 />
               </CCol>
             </CFormGroup>
+
+
             <CFormGroup row>
               <CCol md="3">
-                <CLabel htmlFor="email-input">Enter tags</CLabel>
+                <CLabel htmlFor="email-input">Enter keywords</CLabel>
               </CCol>
               <CCol xs="12" md="9">
-                <CInput type="tag" id="email-input" name="email-input" placeholder="Enter tags" autoComplete="tag"/>
+                <CInput type="tag" id="email-input" name="email-input" placeholder="Enter tags" autoComplete="tag"  value={keywordsInput} onInput={e =>setKeywords(e.target.value)}/>
                 <CFormText className="help-block">Please enter your email</CFormText>
               </CCol>
             </CFormGroup>
@@ -94,30 +108,34 @@ const AddProduct = () => {
                 </CSelect>
               </CCol>
             </CFormGroup>
+
+
             <CFormGroup row>
               <CCol md="3">
                 <CLabel htmlFor="text-input">Price</CLabel>
               </CCol>
               <CCol xs="12" md="9">
-                <CInput id="text-input" name="text-input" placeholder="Text" />
+                <CInput id="text-input" name="text-input" placeholder="Text"  value={priceInput} onInput={e =>setPrice(e.target.value)}/>
                 <CFormText>This is a help text</CFormText>
               </CCol>
             </CFormGroup>
+
+
             <CFormGroup row>
               <CCol md="3">
                 <CLabel htmlFor="text-input">Size</CLabel>
               </CCol>
               <CCol xs="12" md="9">
-                <CInput id="text-input" name="text-input" placeholder="Text" />
+                <CInput id="text-input" name="text-input" placeholder="Text"  value={sizeInput} onInput={e =>setsize(e.target.value)}/>
                 <CFormText>This is a help text</CFormText>
               </CCol>
             </CFormGroup>
             <CFormGroup row>
             <CCol xs="3">
-                    <CLabel htmlFor="cvv">Units</CLabel>
+                    <CLabel htmlFor="quantity">Quantity</CLabel>
                     </CCol>
                     <CCol xs="12" md='3'>
-                    <CInput id="cvv" placeholder="12" required/>
+                    <CInput id="quantity" placeholder="12"  value={quantityInput} onInput={e =>setQuantity(e.target.value)}/>
 
                 </CCol>
             </CFormGroup>
@@ -131,6 +149,7 @@ const AddProduct = () => {
                   name="file-multiple-input" 
                   multiple
                   custom
+                  value={imgInput} onInput={e =>setImg(e.target.value)}
                 />
                 <CLabel htmlFor="file-multiple-input" variant="custom-file">
                   Choose Files...
@@ -149,7 +168,7 @@ const AddProduct = () => {
           </CForm>
         </CCardBody>
         <CCardFooter>
-          <CButton type="submit" size="sm" color="primary"><CIcon name="cil-scrubber" /> Submit</CButton>
+          <CButton type="submit" size="sm" color="primary" onClick={AddProductFetch}><CIcon name="cil-scrubber" /> Submit</CButton>
           <CButton type="reset" size="sm" color="danger"><CIcon name="cil-ban" /> Reset</CButton>
         </CCardFooter>
       </CCard>
