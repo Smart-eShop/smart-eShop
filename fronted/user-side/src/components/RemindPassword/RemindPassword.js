51559d87fcb72aead7faa52f3ef2ad28a83ee5dc
djanuskaite <januskaite.domante@gmail.com>
2021-03-12 10:46:39 +0200

    forgot passw alerts working

--- fronted/user-side/src/components/RemindPassword/RemindPassword.js
+++ fronted/user-side/src/components/RemindPassword/RemindPassword.js
@@ -1,15 +1,15 @@
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-import {Formik, Form, ErrorMessage} from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import FormGroup from '@material-ui/core/FormGroup';
 import Alert from '@material-ui/lab/Alert';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-import {string as yupString, object as yupObject} from 'yup';
+import { string as yupString, object as yupObject } from 'yup';
 
 
 const useStyles = makeStyles(theme => ({
@@ -42,53 +42,54 @@ export default function RemindPassword() {
 
     const passwFetch = e => {
         console.log('lalala');
-           fetch(`https://eshopsmart.herokuapp.com/api/password/email?email=${emailInput}`, {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      }
-      )
-      .then(response => response.json())
-      .then(res => {
-        console.log(res);
-        if(res.status === 200) {
-            setEmailSent(true);
-            setRequestError(false);
-        } else {
-            setRequestError(true);
-            setEmailSent(false);
+        fetch(`https://eshopsmart.herokuapp.com/api/password/email?email=${emailInput}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
         }
-    })
-    .catch(err => {
-        setRequestError(true);
-        setEmailSent(false);
-    })
-    
-    
+        )
+            .then(response => response.json())
+            .then(res => {
+
+                console.log(res.message);
+                if (res.message === "We have emailed your password reset link!") {
+                    setEmailSent(true);
+                    setRequestError(false);
+                } else {
+                    setRequestError(true);
+                    setEmailSent(false);
+                }
+            })
+            .catch(err => {
+                setRequestError(true);
+                setEmailSent(false);
+            })
+
+
     }
     return (
         <div className={classes.root}>
-           <h2>Forgot Password</h2>
-           <h5>Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.</h5>
-            {emailSent? <Alert severity="success">We have emailed your password reset link!</Alert>: null}
-            {requestError? <Alert severity="error">We can't find a user with that email address.</Alert>: null}
+            <h2>Forgot Password</h2>
+            <h5>Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.</h5>
+            {emailSent ? <Alert severity="success">We have emailed your password reset link!</Alert> : null}
+            {requestError ? <Alert severity="error">We can't find a user with that email address.</Alert> : null}
             <Formik
                 initialValues={initialValues}
                 validationSchema={validationSchema}
-               
+
             >
-            { ( {handleChange, handleBlur, isSubmitting, handleSubmit}) => (
-                <Form onSubmit={handleSubmit}>
-                    <FormGroup>
-                        <TextField name='email' label='Email Address' color='primary' variant='outlined' onChange={handleChange} onBlur={handleBlur}  value={emailInput} onInput={e => setEmail(e.target.value)}  fullWidth />
-                        {/* <ErrorMessage name='email' render={msg => <div className="text-danger">{msg}</div>} /> */}
-                    </FormGroup>
-                    <Button type='submit' fullwidth variant='contained' disabled={isSubmitting} color='default' className={classes.submit} onClick={passwFetch}>
-                        Submit
+                {({ handleChange, handleBlur, isSubmitting, handleSubmit }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <FormGroup>
+                            <TextField name='email' label='Email Address' color='primary' variant='outlined' onChange={handleChange} onBlur={handleBlur} value={emailInput} onInput={e => setEmail(e.target.value)} fullWidth />
+                            {/* <ErrorMessage name='email' render={msg => <div className="text-danger">{msg}</div>} /> */}
+                        </FormGroup>
+                        <Button type='submit' fullwidth variant='contained' disabled={isSubmitting} color='default' className={classes.submit} onClick={passwFetch}>
+                            Submit
                     </Button>
-                </Form>
-            )}    
+                    </Form>
+                )}
             </Formik>
         </div>
     )
