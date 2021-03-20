69540c7d78b4adcd5b9fc29f66437a3738b26658
Linas Pelenis <linaspelenis@yahoo.com>
2021-03-20 19:44:56 +0200

    fix route, from deliverty to payment

--- laravel-api/routes/api.php
+++ laravel-api/routes/api.php
@@ -58,7 +58,7 @@ Route::get('delivery/delete/{delivery}', 'DeliveryController@destroy');
 Route::post('payment/store', 'PaymentController@store');
 Route::post('payment/update/{payment}', 'PaymentController@update');
 Route::get('payment/delete/{payment}', 'PaymentController@destroy');
-Route::get('payment/show', 'DeliveryController@showAll');
+Route::get('payment/show', 'PaymentController@showAll');
 
 
 Route::get('recaptcha', 'ApiController@recaptchaKey');
