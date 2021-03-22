c22b21c9bc5243033c8e262776f0b319f27b8b65
laura.sadi <laura.sadinauskaite@gmail.com>
2021-03-22 18:39:55 +0200

    checkout

---
+++ fronted/user-side/src/components/Checkout/AddressForm.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
+
+const useStyles = makeStyles({
+    root: {
+      width: '100%',
+    },
+  });
+
+export default function AddressForm() {
+    const classes = useStyles();
+    const [delivery, setItems] = useState([]);
+    // const [filter, setFilter] = useState("");
+
+    const printItems = async () => {
+        const url = 'https://eshopsmart.herokuapp.com/api/delivery/show';
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data.delivery);
+        setItems(data.Delivery);
+    }
+
+    useEffect(() => {
+        printItems();
+    }, []);
+
+   
+
+  return (
+    <React.Fragment>
+
+      <Typography variant="h6" gutterBottom>
+        Pažymėkite pristatymo būdą
+      </Typography>
+
+      <div className={classes.root}>
+           {delivery.map((item) => (
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-label="Expand"
+          aria-controls="additional-actions1-content"
+          id="additional-actions1-header"
+        >
+          <FormControlLabel
+            aria-label="Acknowledge"
+            onClick={(event) => event.stopPropagation()}
+            onFocus={(event) => event.stopPropagation()}
+            control={<Checkbox />}
+            label={item.name}
+          />
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography color="textSecondary">
+            {item.terms}
+            {item.price}
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      ))}   
+        </div>
+    </React.Fragment>
+          
+    )
+}
--- fronted/user-side/src/components/Main.js
+++ fronted/user-side/src/components/Main.js
@@ -14,6 +14,7 @@ import TermsConditions from "../components/TermsConditions";
 import RemindPassword from './RemindPassword/RemindPassword';
 import ResetPassword from './RemindPassword/ResetPassword';
 import FrontPage from './FrontPage/FrontPage';
+import Checkout from './Checkout/Checkout';
 
 const Main = () => {
        return (
@@ -29,6 +30,7 @@ const Main = () => {
                     <Route path="/about" exact component={About} />
                     <Route path="/category" exact component={ProductsCategory} />
                     <Route path="/contact-us" exact component={Contact} />
+                    <Route path="/checkout" exact component={Checkout} />
                     <Route path="/terms-conditions" exact component={TermsConditions} />
                     <Route path="/remind-password" exact component={RemindPassword} />
                     <Route path="/reset-password" exact component={ResetPassword} />
