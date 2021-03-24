b6f1d752658c42d101b6d5418b91805d0ab0447e
laura.sadi <laura.sadinauskaite@gmail.com>
2021-03-24 11:06:34 +0200

    nauja checkout forma

--- fronted/user-side/src/components/Checkout/AddressForm.js
+++ fronted/user-side/src/components/Checkout/AddressForm.js
@@ -1,58 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import NativeSelect from '@material-ui/core/NativeSelect';
+// import React, { useState, useEffect } from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Typography from '@material-ui/core/Typography';
+// import NativeSelect from '@material-ui/core/NativeSelect';
 
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    display: 'block',
-    marginTop: theme.spacing(2),
-  },
-  formControl: {
-    margin: theme.spacing(0.2),
-    minWidth: 250,
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   button: {
+//     display: 'block',
+//     marginTop: theme.spacing(2),
+//   },
+//   formControl: {
+//     margin: theme.spacing(0.2),
+//     minWidth: 250,
+//   },
+// }));
 
 
-export default function AddressForm() {
+// export default function AddressForm() {
 
 
-  const classes = useStyles();
+//   const classes = useStyles();
 
 
-  const [printDelivery, setPrintDelivery] = useState([]);
-  const printAllDeliveries = async () => {
-    const url = 'https://eshopsmart.herokuapp.com/api/delivery/show';
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data.Delivery);
-    setPrintDelivery(data.Delivery);
-  }
+//   const [printDelivery, setPrintDelivery] = useState([]);
+//   const printAllDeliveries = async () => {
+//     const url = 'https://eshopsmart.herokuapp.com/api/delivery/show';
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data.Delivery);
+//     setPrintDelivery(data.Delivery);
+//   }
 
-  useEffect(() => {
-    printAllDeliveries();
-  }, [])
+//   useEffect(() => {
+//     printAllDeliveries();
+//   }, [])
 
-  const [deliveryValue, setDeliveryValue] = useState('');
+//   const [deliveryValue, setDeliveryValue] = useState('');
 
-  return (
-    <React.Fragment>
+//   return (
+//     <React.Fragment>
 
-      <Typography variant="h6" gutterBottom>
-        Pasirinkite pristatymo būdą
-      </Typography>
+//       <Typography variant="h6" gutterBottom>
+//         Pasirinkite pristatymo būdą
+//       </Typography>
 
-      <div>
-        <NativeSelect id='select' variant='standard' className={classes.formControl}>
-          {printDelivery.map((delivery) => (
-            <option>{delivery.name}</option>
-          ))}
-        </NativeSelect>
+//       <div>
+//         <NativeSelect id='select' variant='standard' className={classes.formControl}>
+//           {printDelivery.map((delivery) => (
+//             <option>{delivery.name}</option>
+//           ))}
+//         </NativeSelect>
 
-      </div>
-    </React.Fragment>
-  );
-}
+//       </div>
+//     </React.Fragment>
+//   );
+// }
 
--- fronted/user-side/src/components/Checkout/PaymentForm.js
+++ fronted/user-side/src/components/Checkout/PaymentForm.js
@@ -1,55 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import NativeSelect from '@material-ui/core/NativeSelect';
+// import React, { useState, useEffect } from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Typography from '@material-ui/core/Typography';
+// import NativeSelect from '@material-ui/core/NativeSelect';
 
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    display: 'block',
-    marginTop: theme.spacing(2),
-  },
-  formControl: {
-    margin: theme.spacing(0.2),
-    minWidth: 250,
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   button: {
+//     display: 'block',
+//     marginTop: theme.spacing(2),
+//   },
+//   formControl: {
+//     margin: theme.spacing(0.2),
+//     minWidth: 250,
+//   },
+// }));
 
 
-export default function PaymentForm() {
-  const classes = useStyles();
+// export default function PaymentForm() {
+//   const classes = useStyles();
 
-  const [printPayment, setPrintPayment] = useState([]);
-  const printAllPayments = async () => {
-    const url = 'https://eshopsmart.herokuapp.com/api/payment/show';
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data.payment);
-    setPrintPayment(data.payment);
-  }
+//   const [printPayment, setPrintPayment] = useState([]);
+//   const printAllPayments = async () => {
+//     const url = 'https://eshopsmart.herokuapp.com/api/payment/show';
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     console.log(data.payment);
+//     setPrintPayment(data.payment);
+//   }
 
-  useEffect(() => {
-    printAllPayments();
-  }, [])
+//   useEffect(() => {
+//     printAllPayments();
+//   }, [])
 
-const [paymentId, setPaymentId]=useState(0);
+// const [paymentId, setPaymentId]=useState(0);
 
-console.log(paymentId)
+// console.log(paymentId)
 
-  return (
-    <React.Fragment>
+//   return (
+//     <React.Fragment>
 
-      <Typography variant="h6" gutterBottom>
-        Pasirinkite apmokėjimo būdą
-      </Typography>
-      <div>
-        <NativeSelect id='select' variant='standard' className={classes.formControl} value={paymentId} onChange={(e) => setPaymentId(e.target.value)}>
-          {printPayment.map((payment) => (
-            <option value={payment.id}>{payment.name}</option>
-          ))}
-        </NativeSelect>
+//       <Typography variant="h6" gutterBottom>
+//         Pasirinkite apmokėjimo būdą
+//       </Typography>
+//       <div>
+//         <NativeSelect id='select' variant='standard' className={classes.formControl} value={paymentId} onChange={(e) => setPaymentId(e.target.value)}>
+//           {printPayment.map((payment) => (
+//             <option value={payment.id}>{payment.name}</option>
+//           ))}
+//         </NativeSelect>
 
-      </div>
-    </React.Fragment>
-  );
-}
+//       </div>
+//     </React.Fragment>
+//   );
+// }
