62f63493b39536511cbd82271957ff1d91f54cc5
martynasali <70946929+martynasali@users.noreply.github.com>
2021-03-26 09:09:00 +0200

    manto puslapiai

--- fronted/user-side/src/components/About/About.js
+++ fronted/user-side/src/components/About/About.js
@@ -1,18 +1,37 @@
-
-import React from 'react';
+import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBView } from "mdbreact";
+import React from "react";
 
 const Cart = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <h1>cart</h1>
-    </div>
+    <MDBCard className="my-5 px-5 pb-5">
+      <MDBCardBody>
+        <h2 className="h1-responsive font-weight-bold text-center my-5">
+        </h2>
+        <br />
+        <MDBRow>
+          <MDBCol lg="5" md="12" sm="12">
+            <MDBView className="rounded z-depth-2 mb-lg-0 mb-4" hover waves>
+              <img
+                className="img-fluid"
+                src="https://images.unsplash.com/photo-1485827404703-89b55fcc595e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                alt=""
+              />
+            </MDBView>
+          </MDBCol>
+          <MDBCol lg="4">
+            <h3 className="font-weight-bold mb-3 p-0">
+              <strong>Mūsų istorija</strong>
+            </h3>
+            <p>
+              Įsivaizduokite pasaulį, kuriame apsipirkimas internetu yra paprastas, smagus ir įkvepiantis. Pasaulį, kuriame nebereikia ieškoti kito geriausio varianto, nes žinote, kad jį jau radote.
+            </p>
+            <p>Mes tikime, kad toks pasaulis egzistuoja čia pat Žemėje, bet iki šiol buvo gerai saugoma paslaptis. Iki šiol. Mūsų misija yra nunešti jus ten, kur jūsų dar nebuvo nunešusi jokia kita interneto parduotuvė, tam, kad galėtumėt patirti apsipirkimo internetu patirtį tokią, kokia ji ir turėtų būti – patogi, greita ir svarbiausia patikima.</p>
+            <p>Smart-eShop tai naujos kartos interneto parduotuvė, siūlanti platų buities technikos ir elektronikos prekių pasirinkimą puikiomis kainomis. Mūsų technikos ekspertų komanda dirba tam, kad kiekvienas mūsų pirkėjas rastų jo individualius poreikius geriausiai atitinkančias prekes. Didžiuojamės kurdami šią inovatyvią interneto platformą, kuria lengva ir malonu naudotis bei kuri nuolat tobulės laikui bėgant.</p>
+          </MDBCol>
+        </MDBRow>
+        <hr className="my-5" />
+      </MDBCardBody>
+    </MDBCard>
   );
 };
 
--- fronted/user-side/src/components/Contact/Contact.js
+++ fronted/user-side/src/components/Contact/Contact.js
@@ -1,17 +1,66 @@
 
 import React from 'react';
+import { MDBRow, MDBCol, MDBIcon } from "mdbreact";
 
 const Contact = () => {
   return (
     <div
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
+        marginTop: "150px",
       }}
     >
-      <h1>Kontaktai</h1>
+      <section className="my-5">
+        <h2 className="h1-responsive font-weight-bold text-center my-5">
+          Kontaktai
+        </h2>
+        <MDBRow>
+          <MDBCol lg="2" className="lg-0 mb-4"></MDBCol>
+          <MDBCol lg="8">
+            <div
+              id="map-container"
+              className="rounded z-depth-1-half map-container"
+              style={{ height: "400px" }}
+            >
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2294.343972240492!2d23.916272251731318!3d54.89688468023656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e72276dd555557%3A0x2bf3519be19be30c!2sKauno%20Informacini%C5%B3%20technologij%C5%B3%20mokykla!5e0!3m2!1slt!2slt!4v1616704480172!5m2!1slt!2slt"
+                title="This is a unique title"
+                width="100%"
+                height="100%"
+                frameBorder="0"
+                style={{ border: 0 }}
+              />
+            </div>
+            <br />
+            <MDBRow className="text-center">
+              <MDBCol md="6">
+                <p tag="a" floating color="blue" className="accent-1">
+                  <MDBIcon icon="phone" />
+                </p>
+                <span>
+                  <strong>Telefonas:</strong>
+                </span>
+                <p>+37060000078</p>
+                <span>
+                  <strong>Darbo laikas:</strong>
+                </span>
+                <p className="mb-md-0">Pirm - Penkt, 8:00-22:00</p>
+              </MDBCol>
+              <MDBCol md="6">
+                <p tag="a" floating color="blue" className="accent-1">
+                  <MDBIcon icon="envelope" />
+                </p>
+                <span>El.Paštas:</span>
+                <p title="El.Paštas">
+                  <strong>smart-eshop@gmail.com</strong>
+                </p>
+                <p className="mb-md-0">
+                  <strong>pardavimai@gmail.com</strong>
+                </p>
+              </MDBCol>
+            </MDBRow>
+          </MDBCol>
+        </MDBRow>
+      </section>
     </div>
   );
 };
